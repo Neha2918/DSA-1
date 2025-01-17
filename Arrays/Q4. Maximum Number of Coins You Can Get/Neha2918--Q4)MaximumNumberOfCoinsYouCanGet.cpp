2d935239ad7d3c055a2565999707080132c2e62b
Neha2918 <nehaashodiya@gmail.com>
2025-01-17 21:52:57 +0530

    cpp solution for issue #39

--- Arrays/Q4. Maximum Number of Coins You Can Get/Neha2918--Q4)MaximumNumberOfCoinsYouCanGet.cpp
+++ Arrays/Q4. Maximum Number of Coins You Can Get/Neha2918--Q4)MaximumNumberOfCoinsYouCanGet.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
-       int n = piles.size() ;
+        // Get the total number of piles
+        int n = piles.size();
 
-       sort(begin(piles),end(piles));
+        // Sort the piles in ascending order to maximize our pick later
+        sort(begin(piles), end(piles));
 
-       int result = 0;
+        int result = 0; // To store the total coins we collect
 
-       int Bob = 0;
-       int M = n-2;
+        int Bob = 0;    // Pointer representing Bob's turn (smallest piles)
+        int M = n - 2;  // Pointer to our turn (second largest pile)
 
-       while(M>Bob){
-        result+=piles[M];
+        // Iterate while there are still piles for Bob
+        while (M > Bob) {
+            result += piles[M]; // Add the second largest pile to our total
 
-        M-=2;
-        Bob++;
-       }
-       return result;
+            M -= 2; // Move to the next "second largest" pile
+            Bob++;  // Move Bob's pointer to the next smallest pile
+        }
+
+        return result; // Return the total coins collected
     }
 };
